b211e221f1234cdab6f5ff049957f7ad4c0cc95a
Alice Liu <alice@Ranias-MacBook-Pro.local>
2021-06-16 14:04:42 +0200

    Updates

---
+++ precache-manifest.9b075cac8ec9ccad03cb18b747355ea0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d418f2020e4e2aa735f71a06e05425",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "23d38c46b00ca4b8442c",
+    "url": "/blindtest-spotify/static/css/main.348ad1c0.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d38c46b00ca4b8442c",
+    "url": "/blindtest-spotify/static/js/main.b83356aa.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  }
+]);
